d990617a756d17bb86f64465b1624062eb93f790
Ahmad raza <119778503+Ahmad5626@users.noreply.github.com>
2024-11-02 13:15:31 +0530

    first commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+php<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://avconstructionsindia.in</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://avconstructionsindia.in/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://avconstructionsindia.in/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://avconstructionsindia.in/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://avconstructionsindia.in/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://avconstructionsindia.in/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
